1bed15426bac51a42031da5a1f1a4854b6362a10
anton-serhiienko <manton.ssergienko@gmail.com>
2021-03-03 17:44:58 +0200

    Initial commit

---
+++ src/components/Header.jsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+const Header = () => {
+    return(
+        <header className="header">
+            <img src={'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSCCmV4hdR0WwQdDksdPtHdYPSGo27bnOl0dA&usqp=CAU'}/>
+        </header>
+    );
+}
+
+export default Header;
