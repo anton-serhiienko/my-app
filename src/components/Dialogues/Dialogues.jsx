9383ff1bbfac17a4c7467868d6c3d9a814276cd9
anton-serhiienko <manton.ssergienko@gmail.com>
2021-03-06 18:05:23 +0200

    06.03.2021 my-app 19 lesson

---
+++ src/components/Dialogues/Dialogues.jsx
@@ -0,0 +1,12 @@
+// import css from "./Music.module.css";
+
+
+const Dialogues = () => {
+    return(
+        <div>
+            Dialogues
+        </div>
+    )
+}
+
+export default Dialogues;
--- src/components/Navbar/Navbar.jsx
+++ src/components/Navbar/Navbar.jsx
@@ -3,22 +3,22 @@ import css from "./Navbar.module.css"
 
 
 const Navbar = () => {
-    return(
+    return (
         <nav className={css.nav}>
             <div className={css.item}>
-                <a>Profile</a>
+                <a href="/profile">Profile</a>
             </div>
             <div className={css.item}>
-                <a>Messages</a>
+                <a href="/dialogues">Messages</a>
             </div>
             <div className={css.item}>
-                <a>News</a>
+                <a href="/news">News</a>
             </div>
             <div className={css.item}>
-                <a>Music</a>
+                <a href="/music">Music</a>
             </div>
             <div className={css.item}>
-                <a>Settings</a>
+                <a href="/settings">Settings</a>
             </div>
 
         </nav>
