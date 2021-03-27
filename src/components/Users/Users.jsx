a80ff5094b5fafd3fe2d52a8507a152272644273
anton-serhiienko <manton.ssergienko@gmail.com>
2021-03-27 20:44:51 +0200

    27.03.2021 my-app

--- src/components/Users/Users.jsx
+++ src/components/Users/Users.jsx
@@ -2,6 +2,21 @@ import React from 'react';
 import styles from "./users.module.css"
 
 let Users = (props) => {
+    if(props.users.length === 0){
+        props.setUsers([
+            {id: 1, photoUrl:'https://lh3.googleusercontent.com/gK5cSYiIvIF3S5DS_3NG_-ZbSTUvwnSkfFr2wFSWjiqtcgaUiOCXJqZTwXL2spKX_hc',
+                followed: false, fullName: "Anton", status:"I'm a boss", location:{city:"Kyiv", country: "Ukraine"}},
+            {id: 2, photoUrl:'https://lh3.googleusercontent.com/gK5cSYiIvIF3S5DS_3NG_-ZbSTUvwnSkfFr2wFSWjiqtcgaUiOCXJqZTwXL2spKX_hc',
+                followed: true, fullName: "Denis", status:"I'm a boss", location:{city:"Tokyo", country: "Japan"}},
+            {id: 3, photoUrl:'https://lh3.googleusercontent.com/gK5cSYiIvIF3S5DS_3NG_-ZbSTUvwnSkfFr2wFSWjiqtcgaUiOCXJqZTwXL2spKX_hc',
+                followed: true, fullName: "Vladimir", status:"I'm a boss", location:{city:"Antalya", country: "Turkey"}},
+            // {id: 4, followed: false, fullName: "Oleh", status:"I'm a boss", location:{city:"Kharkiv", country: "Ukraine"}},
+            // {id: 5, followed: false, fullName: "Ilya", status:"I'm a boss", location:{city:"Barcelona", country: "Spain"}},
+            // {id: 6, followed: true, fullName: "Yaroslav", status:"I'm a boss", location:{city:"Mahachkala", country: "Dagestan"}},
+        ])
+    }
+
+
     return <div>
         {
             props.users.map(u => <div key={u.id}>
--- src/redux/usersReducer.js
+++ src/redux/usersReducer.js
@@ -3,17 +3,7 @@ const UNFOLLOW = "UNFOLLOW";
 const SET_USERS = "SET-USERS";
 
 let initialState = {
-    users: [
-        {id: 1, photoUrl:'https://lh3.googleusercontent.com/gK5cSYiIvIF3S5DS_3NG_-ZbSTUvwnSkfFr2wFSWjiqtcgaUiOCXJqZTwXL2spKX_hc',
-            followed: false, fullName: "Anton", status:"I'm a boss", location:{city:"Kyiv", country: "Ukraine"}},
-        {id: 2, photoUrl:'https://lh3.googleusercontent.com/gK5cSYiIvIF3S5DS_3NG_-ZbSTUvwnSkfFr2wFSWjiqtcgaUiOCXJqZTwXL2spKX_hc',
-            followed: true, fullName: "Denis", status:"I'm a boss", location:{city:"Tokyo", country: "Japan"}},
-        {id: 3, photoUrl:'https://lh3.googleusercontent.com/gK5cSYiIvIF3S5DS_3NG_-ZbSTUvwnSkfFr2wFSWjiqtcgaUiOCXJqZTwXL2spKX_hc',
-            followed: true, fullName: "Vladimir", status:"I'm a boss", location:{city:"Antalya", country: "Turkey"}},
-        // {id: 4, followed: false, fullName: "Oleh", status:"I'm a boss", location:{city:"Kharkiv", country: "Ukraine"}},
-        // {id: 5, followed: false, fullName: "Ilya", status:"I'm a boss", location:{city:"Barcelona", country: "Spain"}},
-        // {id: 6, followed: true, fullName: "Yaroslav", status:"I'm a boss", location:{city:"Mahachkala", country: "Dagestan"}},
-    ],
+    users: [ ]
 }
 
 const usersReducer = (state = initialState, action) => {
